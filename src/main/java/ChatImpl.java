5a77a09f02f2b5f8f49ae3095d9681612d164ecc
Frehiwot Gemechu <frehiwotgem@gmail.com>
2024-12-28 02:46:14 +0300

    .env

--- src/main/java/ChatImpl.java
+++ src/main/java/ChatImpl.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatImpl extends UnicastRemoteObject implements Chat {
-    private List<Chat> clients;
+    final List<Chat> clients;
 
     protected ChatImpl() throws RemoteException {
         clients = new ArrayList<>();
